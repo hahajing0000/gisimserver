83db3a865c9b3118a2a6702d4a9ab8ee8328a8bd
hahajing <444511958@qq.com>
2021-04-16 13:47:16 +0800

    切换回jar包方式部署

--- src/main/java/com/zy/gisim/GisimApplication.java
+++ src/main/java/com/zy/gisim/GisimApplication.java
@@ -8,14 +8,14 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 
 @MapperScan("com.zy.gisim.mapper")
 @SpringBootApplication
-public class GisimApplication extends SpringBootServletInitializer {
+public class GisimApplication  {//extends SpringBootServletInitializer
 
     public static void main(String[] args) {
         SpringApplication.run(GisimApplication.class, args);
     }
 
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-        return builder.sources(GisimApplication.class);//super.configure(builder);
-    }
+//    @Override
+//    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+//        return builder.sources(GisimApplication.class);//super.configure(builder);
+//    }
 }
